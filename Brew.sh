516a7fd9a289114d545c81c8a79e2b70693bc421
Web4 <167559384+Web4application@users.noreply.github.com>
2025-12-01 16:39:49 +0100

    Update Brew.sh

--- Brew.sh
+++ Brew.sh
@@ -1,1 +1,15 @@
 
+# Install OCaml dependencies
+make deps
+
+# Build all executables
+make
+
+# Run all tests
+make test
+
+# Generate coverage report
+make coverage
+
+# Clean build artifacts
+make clean
