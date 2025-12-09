dad9d43ca59ad695f836bea09a4c31602c9ea4ca
Web4 <167559384+Web4application@users.noreply.github.com>
2025-12-09 23:20:40 +0100

    Update and rename mo_doc.ml to src/mo_doc.mli

--- src/mo_doc.mli
+++ src/mo_doc.mli
@@ -2,7 +2,6 @@ let source : string ref = ref "src"
 let output : string ref = ref "docs"
 let format : Docs.output_format ref = ref Docs.Html
 let package : string option ref = ref None
-
 let set_source s = source := s
 let set_output o = output := o
 let set_format = function
