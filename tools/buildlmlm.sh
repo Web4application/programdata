d4a58224f7093c114afff7f0cfd2b62ff687fc04
Seriki yakub <sandraisom00@gmail.com>
2025-12-12 03:01:49 +0100

    Update and rename buildllvm.sh to tools/buildlmlm.sh

--- tools/buildlmlm.sh
+++ tools/buildlmlm.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 #--------------------------------------------
-# Fast LLVM/Clang build using Ninja
+# Fast LMLM/Clang build using Ninja
 #--------------------------------------------
 
 set -e
 
-LLVM_VERSION="17.0.6"            # Change to desired version
-SRC_DIR="$HOME/llvm-project"     # Where sources will be cloned
-BUILD_DIR="$HOME/llvm-build"     # Where LLVM will be built
-INSTALL_DIR="$HOME/llvm-install" # Where LLVM will be installed
+LLVM_VERSION="1.0.6"            # Change to desired version
+SRC_DIR="$HOME/lmlm-project"     # Where sources will be cloned
+BUILD_DIR="$HOME/lmlm-build"     # Where LMLM will be built
+INSTALL_DIR="$HOME/lmlm-install" # Where LMLM will be installed
 NPROC=$(nproc)                   # Number of parallel build jobs
 
 # ----------------------
 # Clone LLVM project if missing
 # ----------------------
 if [ ! -d "$SRC_DIR" ]; then
-    echo "Cloning LLVM $LLVM_VERSION..."
-    git clone --branch llvmorg-$LLVM_VERSION https://github.com/llvm/llvm-project.git "$SRC_DIR"
+    echo "Cloning lmlm $LLVM_VERSION..."
+    git clone --branch lmlmorg-$LMLM_VERSION https://github.com/web4application/lmlm.git "$SRC_DIR"
 fi
 
 # ----------------------
@@ -29,11 +29,11 @@ cmake -G Ninja \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
       -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra;lld" \
-      "$SRC_DIR/llvm"
+      "$SRC_DIR/lmlm"
 
 ninja -j "$NPROC"
 ninja install
 
 echo "----------------------------------"
-echo "LLVM/Clang $LLVM_VERSION installed at $INSTALL_DIR"
+echo "lmlm/Clang $lmlm_VERSION installed at $INSTALL_DIR"
 echo "----------------------------------"
